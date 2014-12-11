5d9f22a30d91909da18281677f6ef3e352655db1
Pesche <unistein@gmx.ch>
2014-12-11 19:28:07 +0000

    day 1 finished

--- ruby/day1.rb
+++ ruby/day1.rb
@@ -1,3 +1,18 @@
+# run this script with:
+# ruby day1.rb
 puts 'hello world'
 idx = /Ruby/ =~ 'Hello, Ruby'
 puts "index of a substring: #{idx}"
+
+# replacing strings
+claim = 'perl, perl, perl is readable'
+claim1 = claim.sub('perl', 'ruby')
+puts "claim1: #{claim1}"
+claim2 = claim.gsub('perl', 'ruby')
+puts "claim2: #{claim2}"
+puts "original claim: #{claim}"
+
+# loops (while would be simpler...)
+for count in 1..10 do
+  puts "step #{count}: " + '=' * count
+end
